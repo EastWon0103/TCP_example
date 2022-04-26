e053af2b7ad8c392a1876f3c4b35c1f6dbc92eeb
김동원 <dongwon0103@naver.com>
2022-04-26 18:55:13 +0900

    client go

--- http_client.py
+++ http_client.py
@@ -1,10 +1,9 @@
-from socket import *
 import http.client
 import json
 
 
-HOST = '125.240.96.241'
-PORT = 9999
+HOST = "125.240.96.241"
+PORT = 12345
 
 conn = http.client.HTTPConnection(HOST, PORT)
 headers = {"Content-type":"text/html"}
@@ -87,33 +86,3 @@ except Exception as e:
     
 finally:
     conn.close()
-# r = conn.getresponse()
-# print(r.status, r.reason)
-#print(r.read().decode("utf-8"))
-
-# try:  
-#     while True:
-#         cmd = input("Request: ")
-        
-#         request = cmd.split(" ")
-#         method = request[0].upper()
-#         data = request[1:]
-
-#         headers = {"Content-type":"aplication/json"}
-
-
-#         if(len(request) == 1):
-#             conn.request(method, "/")
-#         else:
-#             conn.request(method, "/", data, headers) 
-
-
-        
-
-# except KeyboardInterrupt:
-#     print("KeyboardInterrupt: Shutting down the Client")
-
-# finally:
-#     conn.close()
-
-# conn.close()
