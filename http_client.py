860448d06040f48825b80f76e3328aead6f09a9c
DongwonKim <dongwon0103@naver.com>
2022-04-26 16:29:04 +0900

    finished

--- http_client.py
+++ http_client.py
@@ -1,14 +1,92 @@
 from socket import *
 import http.client
+import json
 
 
 HOST = '125.240.96.241'
-PORT = 12345
+PORT = 9999
 
 conn = http.client.HTTPConnection(HOST, PORT)
-headers = {"Content-type":"aplication/json"}
-data = {"data1":123}
-conn.request("POST", "/", str(data), headers)
+headers = {"Content-type":"text/html"}
+# data = {"data1":123}
+# conn.request("POST", "/", str(data), headers)
+
+try:
+    while True:
+        cmd = input("명령어 입력: ")
+        cmd = cmd.upper()
+        
+        # HTTP REQUEST: GET
+        # html을 텍스트로 돌려받고 터미널에서 todolist를 확인가능        
+        if cmd == "GET":
+            conn.request(cmd, "/")
+            r = conn.getresponse()
+            print(r.status, r.reason)
+            toDoList = []
+            for i in r.readlines():
+                txt = i.decode("utf-8")
+                print(txt, end="")         
+                if txt.strip()[:4] == "<li>":
+                    txt = txt.strip()[4:]
+                    txt = txt.strip()[:len(txt)-5]
+                    toDoList.append(txt)
+                    
+            print("\n")
+            print("To Do List")
+            for todo in toDoList:
+                print("* "+todo)    
+            print()
+        
+        # HTTP REQUEST: HEAD
+        # header의 간단한 부분만 살펴볼 수 있다.  
+        elif cmd == "HEAD":
+            conn.request(cmd, "/")
+            r = conn.getresponse()
+            print(r.headers)
+        
+        # HTTP REQUEST: POST
+        # 데이터 등록하기 
+        elif cmd == "POST":
+            data = input("등록할 데이터: ")
+            conn.request(cmd, "/", data.encode("utf-8"),headers)
+            r = conn.getresponse()
+            print(r.status, r.reason)
+            print(r.read().decode("utf-8"))
+            print()
+
+        elif cmd == "DELETE":
+            data = input("삭제할 데이터: ")
+            conn.request(cmd, "/", data.encode("utf-8"),headers)
+            r = conn.getresponse()
+            print(r.status, r.reason)
+            print(r.read().decode("utf-8"))
+            print()
+        
+        elif cmd == "PUT":
+            mData = input("수정할 데이터: ")
+            nData = input("바꿀 내용: ")
+            
+            data = {"old": mData, "new": nData}
+            
+            conn.request(cmd, "/", json.dumps(data).encode("utf-8"), {"Content-type":"application/json"})
+            r = conn.getresponse()
+            print(r.status, r.reason)
+            print(r.read().decode("utf-8"))
+            print()
+            
+        else:
+            print("잘못된 명령어")
+            print()
+            continue
+
+except KeyboardInterrupt:
+    print("KeyboardInterrupt: 서버를 종료합니다.")
+
+except Exception as e:
+    print(e, ": shutting down client program")
+    
+finally:
+    conn.close()
 # r = conn.getresponse()
 # print(r.status, r.reason)
 #print(r.read().decode("utf-8"))
@@ -38,4 +116,4 @@ conn.request("POST", "/", str(data), headers)
 # finally:
 #     conn.close()
 
-conn.close()
+# conn.close()
--- http_server.py
+++ http_server.py
@@ -1,50 +1,134 @@
 import http.server
 import socketserver
 import codecs
+import json
 HOST = "125.240.96.241"
-PORT = 12345
+PORT = 9999
 
 class MyHandler(http.server.BaseHTTPRequestHandler):
-    
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html; charset=utf-8")
+        self.send_header("Host IP", HOST)
+        self.send_header("Port", str(PORT))
+        self.end_headers()
+        
     def do_GET(self):
         html_file = codecs.open("index.html", "r", "utf-8")
         lines = html_file.readlines()
         
         try:
-            self.send_response(200)
-            self.send_header("Content-Type", 'text/html; charset=utf-8')
-            self.end_headers()
+            self._set_headers()
             self.wfile.write("".join(lines).encode("utf-8"))
-        except Exception:
+        except Exception as e:
             print(self.error_message_format)
             print(self.error_content_type)
-            print("error")
+            print(e)
         finally:
             html_file.close()
     
     def do_HEAD(self):
         try:
-            self.send_response(200)
-            self.send_header("Content-Type", 'text/html; charset=utf-8')
-            self.end_headers()
-        except:
-            print("hi")
+            self._set_headers()
+            self.wfile.write("".encode("utf-8"))
+        except Exception as e:
+            print(self.error_message_format)
+            print(self.error_content_type)
+            print(e)
             
     def do_POST(self):
-        self.send_response(200)
-        self.send_header("Content-Type", "text/html")
-        self.end_headers()
-        
-        data_legth = int(self.headers["Content-Length"])
-        data = self.rfile.read(data_legth)
-        print(type(data))    
-    
+        self._set_headers()
+        try:
+            data_legth = int(self.headers["Content-Length"])
+            data = self.rfile.read(data_legth)
+            data = data.decode("utf-8")
+            print("받은 데이터: ",data)
+            html_data = "                <li>"+data+"</li>\r\n"
+            
+            f = codecs.open("index.html", "r", "utf-8")
+            lines = f.readlines()
+            f.close()
+            
+            f = codecs.open("index.html", "w", "utf-8")
+            postable = False
+            for i in lines:
+                if i.strip() == "</ul>":
+                    lines.insert(lines.index(i), html_data)
+                    break
+                
+            f.writelines(lines)
+            message = "<" + data +">" + " 등록완료"
+            
+        except Exception as e:
+            print(self.error_message_format)
+            print(self.error_content_type)
+            print(e)
+            message = "<" + data +">" + " 등록실패"
+            
+        finally:
+            self.wfile.write(message.encode("utf-8"))
+            f.close()
+                
     def do_PUT(self):
-        self.send_response(200)
+        self._set_headers()
+        try:
+            data_legth = int(self.headers["Content-Length"])
+            data = self.rfile.read(data_legth)
+            data = data.decode("utf-8")
+            data = json.loads(data)
+            print("받은 데이터: ",data["old"], data["new"])
+            target_data = "                <li>"+data["old"]+"</li>\r\n"
+            modify_data = "                <li>"+data["new"]+"</li>\r\n"
+            
+            f = codecs.open("index.html", "r", "utf-8")
+            lines = f.readlines()
+            f.close()
+            
+            target_index = lines.index(target_data)
+            lines[target_index] = modify_data
+
+            f = codecs.open("index.html", "w", "utf-8")
+            f.writelines(lines)
+            message = "<" + data["old"] +" "+"->"+" " + data["new"] +">" + " 수정완료"
+        
+        except Exception as e:
+            print(self.error_message_format)
+            print(self.error_content_type)
+            print(e)
+            message = "<" + data["old"] +" "+"->"+" " + data["new"] +">" + " 수정실패"
+            
+        finally:
+            self.wfile.write(message.encode("utf-8"))
+            f.close()
     
     def do_DELETE(self):
-        self.send_response(200)
-    
+        self._set_headers()
+        try:
+            data_legth = int(self.headers["Content-Length"])
+            data = self.rfile.read(data_legth)
+            data = data.decode("utf-8")
+            print("받은 데이터: ",data)
+            target_data = "                <li>"+data+"</li>\r\n"
+            
+            f = codecs.open("index.html", "r", "utf-8")
+            lines = f.readlines()
+            f.close()
+            
+            lines.remove(target_data)
+            
+            f = codecs.open("index.html", "w", "utf-8")
+            f.writelines(lines)
+            message = "<" + data +">" + " 삭제완료"
+        
+        except Exception as e:
+            print(self.error_message_format)
+            print(self.error_content_type)
+            print(e)
+            message = "<" + data +">" + " 삭제실패"
+            
+        finally:
+            self.wfile.write(message.encode("utf-8"))
+            f.close()
     
 
 with socketserver.TCPServer((HOST, PORT), MyHandler) as httpd:
